f524398294195f0fbd569264d697f0071c8ef596
Frank Meyerer <frank.meyerer@ueberbit.de>
2015-08-27 23:01:14 +0200

    Initial placeholder page

---
+++ src/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8">
+    <title>MRN.Webkickern 2015 - Ein Event des Webmontag WMMRN</title>
+    <link rel="stylesheet" href="/css/style.css">
+  </head>
+  <body>
+    <div class="info-wrapper">
+      <div class="info">
+      	<h1>MRN.Webkickern 2015</h1>
+      	<h3>12. Oktober 2015 - More infos soon</h3>
+      </div>
+    </div>
+  </body>
+</html>
